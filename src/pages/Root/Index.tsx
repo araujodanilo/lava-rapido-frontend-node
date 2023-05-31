8ec02599fcc0edc9ec1086967d3aa411e2d9bf95
Danilo Araujo <x45danilo45x@gmail.com>
2023-05-31 18:29:18 -0300

    feat: root/index page

---
+++ src/pages/Root/Index.tsx
@@ -0,0 +1,11 @@
+import TemplatePage from "../TemplatePage";
+
+const RootPage = () => {
+    return (
+        <TemplatePage>
+            <h1>Index</h1>
+        </TemplatePage>
+    )
+}
+
+export default RootPage;
